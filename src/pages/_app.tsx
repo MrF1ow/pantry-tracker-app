0a4a6ac613bb714e841f5a4c09fdefb6aad6a758
MrF1ow <eflow1280@gmail.com>
2024-08-04 18:56:09 -0700

    Fixed Lint Issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,13 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ReduxProvider>
       <Head>
         <title>PantryPal</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
-          rel="stylesheet"
-        />
+        <meta name="description" content="Welcome to PantryPal, your ultimate inventory management solution." />
       </Head>
       <ThemeProvider theme={lightTheme}>
         <CssBaseline />
---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
